6c96da2e0dacd03a8a76509f65c9f419b815ee15
Benjamin Bustamante <benjamin.bustamante@usach.cl>
2023-10-01 22:20:05 -0300

    Más comentarios a funciones
    Se sigue el proceso de comentar el código.

--- lab1.rkt
+++ lab1.rkt
@@ -1,55 +1,87 @@
 #lang racket
-
+#|
+Nombre de la función: id-option.
+Dominio: option.
+Recorrido: id del option.
+Descripción: Funcion la cual toma la option y retorna la id asociada.|#
 (define id-option
   (lambda (option)
     (car option)))
-
+#|
+Nombre de la función: message-option.
+Dominio: option.
+Recorrido: menssage del option.
+Descripción: Esta función permite retornar el message asociado a la option ingresada.|#
 (define message-option
   (lambda (option)
     (cadr option)))
-
+#|
+Nombre de la función: ChatbotCodeLink-option.
+Dominio: option.
+Recorrido: ChatbotCodeLink del option.
+Descripción: La función permite devolver el ChatbotCodeLink de una option que se le ingrese.|#
 (define ChatbotCodeLink-option
   (lambda (option)
     (caddr option)))
-
+#|
+Nombre de la función: FlowCodeLink-option.
+Dominio: option.
+Recorrido: FlowCodeLink del option.
+Descripción: La función toma una option como argumento y devuelve el FlowCodeLink asociado a esa opción.|#
 (define FlowCodeLink-option
   (lambda (option)
     (cadddr option)))
-
+#|
+Nombre de la función: Keyword-option.
+Dominio: option.
+Recorrido: Keyword del option.
+Descripción: Esta funcion toma como arguemento una option y entrega las palabras clave o "Keywords" asociadas.|#
 (define Keyword-option
   (lambda (option)
     (car(cddddr option))))
 #|
-Nombre de la función: option
-Dominio: code x message x ChatbotLink x FlowCodeLink x Keyword
-Recorrido: option
+Nombre de la función: option.
+Dominio: code x message x ChatbotLink x FlowCodeLink x Keyword.
+Recorrido: option.
 Descripción: Esta función permite crear una representación de una opcion, la cual recibe un codigo, mensaje , codigo del chatbot , un codigo del flow y cero
-o más palabras clave|#
+o más palabras clave.|#
 (define option
   (lambda(code message ChatbotCodeLink FlowCodeLink . Keyword)
     (list code message ChatbotCodeLink FlowCodeLink Keyword)))
 #|
-Nombre de la función: flow 
-Dominio: id x name x option
-Recorrido: flow
+Nombre de la función: flow.
+Dominio: id x name x option.
+Recorrido: flow.
 Descripción: Función la cual permite crear un flow que toma como parametros un id, su nombre y 0 o más opciones. Acá , se verifica
 que no se entreguen opciones repetidas y se realiza dicha verificación en base a la id que posee la opcion llamando a una función recursiva de
-tipo natural|#
+tipo natural.|#
 (define flow
   (lambda(id name . option)
     (cond
       [(null? option) (list id name (list ))]
       [(= (length option) 1) (list id name option)]
       [else (list id name (option-dup(remove-duplicates option) (list )))])))
-
+#|
+Nombre de la función: flow-id.
+Dominio: flow.
+Recorrido: id del flow.
+Descripción: Funcion la cual , en base a la entrega de un flow, retorna su id.|#
 (define flow-id
   (lambda (flow)
     (car flow)))
-
+#|
+Nombre de la función: flow-name
+Dominio: flow
+Recorrido: name del flow
+Descripción: La funcion toma como argumento un flow y retorna el nombre de dicho parametro de entrada.|#
 (define flow-name
   (lambda (flow)
     (cadr flow)))
-
+#|
+Nombre de la función: flow-option
+Dominio: flow
+Recorrido: option del flow
+Descripción: Esta función toma un flow como argumento y retorna las options que poseea el flow.|#
 (define flow-option
   (lambda (flow)
     (caddr flow)))
