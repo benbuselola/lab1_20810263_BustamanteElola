f2be76ab484bce8b3d109dd2c0c70807d3847c62
Benjamin Bustamante <benjamin.bustamante@usach.cl>
2023-10-03 00:27:42 -0300

    Comentarios
    Más comentarios

--- lab1.rkt
+++ lab1.rkt
@@ -211,10 +211,11 @@ Descripción: La función toma el chatbot y entrega la lista de flows asociada.|
   (lambda (chatbot)
     (car(cddddr chatbot))))
 #|
-Nombre de la función: chatbot-flows.
-Dominio: chatbot.
-Recorrido: flows asociados al chatbot.
-Descripción: La función toma el chatbot y entrega la lista de flows asociada.|#
+Nombre de la función: system.
+Dominio: name x InitialChatbotCodeLink x chatbot.
+Recorrido: system.
+Descripción: Función la cual crea el system, que tiene como parametros de entrada el nombre, el InitialChatbotCodeLink y 0 o más chatbots. Con estos
+argumentos se crea una lista representando un system.|#
 (define system
   (lambda (name InitialChatbotCodeLink . chatbot)
     (cond
@@ -222,7 +223,13 @@ Descripción: La función toma el chatbot y entrega la lista de flows asociada.|
       [(= (length chatbot) 1) (list name InitialChatbotCodeLink (list ) (list ) (list ) chatbot)]
       [else (list name InitialChatbotCodeLink (list ) (list ) (list ) (chatbot-dup chatbot (list )))])))
 
-
+#|
+Nombre de la función: system-add-chatbot.
+Dominio: system x chatbot.
+Recorrido: system.
+Descripción: Funcion la cual agrega un chatbot al system. Para verificar la no repetición de chatbots,
+se tiene que verificar por la id del chatbot a ingresar y ver que no se repita. Si se repite el id,
+se retorna el system sin modificar. Caso contrario, se añade el chatbot a la lista de chatbots que posee el system.|#
 (define system-add-chatbot
   (lambda (system chatbot)
     (if (boolean?(member (chatbot-chatbotid chatbot) (map chatbot-chatbotid (system-chatbot system))))
@@ -240,6 +247,11 @@ Descripción: La función toma el chatbot y entrega la lista de flows asociada.|
 (define user
   (lambda (name)
     name))
+#|
+Nombre de la función: system-add-user.
+Dominio: system x user.
+Recorrido: system.
+Descripción: |#
 (define system-add-user
   (lambda (system user)
     (if (boolean? (member user (system-userList system)))
